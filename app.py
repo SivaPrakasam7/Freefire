c71b374155149431c923a943a2b5906add376317
SivaPrakasam7 <prakasams22@gmail.com>
2021-06-09 17:03:41 +0530

    mod

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ import sqlite3
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/<name>",methods=['GET','POST'])
+def login(name):
+    return render_template(f'{name}.html')
+
+
+# google account show profileIdentifier
 
 
 if __name__=="__main__":
